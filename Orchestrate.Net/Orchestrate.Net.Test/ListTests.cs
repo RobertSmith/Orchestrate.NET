aa7f8cbc15258e83fef2aafef6626e3cf696a526
Robert Smith <robert.smith@thumpter.com>
2014-02-13 21:16:12 -0500

    moved list and ref tests into their own test classes

---
+++ Orchestrate.Net/Orchestrate.Net.Test/ListTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orchestrate.Net.Test
+{
+    [TestClass]
+    public class ListTests
+    {
+        const string ApiKey = "<API KEY>";
+        private const string CollectionName = "ListTestCollection";
+        private Orchestrate _orchestrate;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var orchestrate = new Orchestrate(ApiKey);
+
+            var item = new TestData { Id = 1, Value = "Inital Test Item" };
+            var item2 = new TestData { Id = 2, Value = "Inital Test Item #2" };
+            var item3 = new TestData { Id = 3, Value = "Inital Test Item #3" };
+
+            orchestrate.CreateCollection(CollectionName, "1", item);
+            orchestrate.Put(CollectionName, "2", item2);
+            orchestrate.Put(CollectionName, "3", item3);
+
+            context.Properties.Add("Orchestrate", orchestrate);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanUp()
+        {
+            var orchestrate = new Orchestrate(ApiKey);
+            orchestrate.DeleteCollection(CollectionName);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _orchestrate = new Orchestrate(ApiKey);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // nothing to see here...
+        }
+
+        #region List Tests
+
+        [TestMethod]
+        public void List()
+        {
+            var result = _orchestrate.List(CollectionName, 10, null, null);
+            Assert.IsTrue(result.Count > 0);
+        }
+
+        [TestMethod]
+        public void ListWithStartKey()
+        {
+            var result = _orchestrate.List(CollectionName, 10, "1", null);
+            Assert.IsTrue(result.Count > 0);
+        }
+
+        [TestMethod]
+        public void ListWithAfterKey()
+        {
+            var result = _orchestrate.List(CollectionName, 10, null, "1");
+            Assert.IsTrue(result.Count > 0);
+        }
+
+        [TestMethod]
+        public void ListWithStartKeyAndAfterKey()
+        {
+            try
+            {
+                _orchestrate.List(CollectionName, 10, "1", "2");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ParamName == "startKey");
+                return;
+            }
+
+            Assert.Fail("No Exception Thrown");
+        }
+
+        [TestMethod]
+        public void ListWithNoCollectionName()
+        {
+            try
+            {
+                _orchestrate.List(string.Empty, 10, null, null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.IsTrue(ex.ParamName == "collectionName");
+                return;
+            }
+
+            Assert.Fail("No Exception Thrown");
+        }
+
+        [TestMethod]
+        public void ListWithLimitOutOfBounds()
+        {
+            try
+            {
+                _orchestrate.List(CollectionName, -110, null, null);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsTrue(ex.ParamName == "limit");
+                return;
+            }
+
+            Assert.Fail("No Exception Thrown");
+        }
+
+        #endregion
+    }
+}
---
+++ Orchestrate.Net/Orchestrate.Net.Test/RefTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orchestrate.Net.Test
+{
+    [TestClass]
+    public class RefTests
+    {
+        const string ApiKey = "<API KEY>";
+        private const string CollectionName = "RefTestCollection";
+        private Orchestrate _orchestrate;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var orchestrate = new Orchestrate(ApiKey);
+
+            var item = new TestData { Id = 1, Value = "Inital Test Item" };
+            var item2 = new TestData { Id = 2, Value = "Inital Test Item #2" };
+            var item3 = new TestData { Id = 3, Value = "Inital Test Item #3" };
+
+            orchestrate.CreateCollection(CollectionName, "1", item);
+            orchestrate.Put(CollectionName, "2", item2);
+            orchestrate.Put(CollectionName, "3", item3);
+
+            context.Properties.Add("Orchestrate", orchestrate);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanUp()
+        {
+            var orchestrate = new Orchestrate(ApiKey);
+            orchestrate.DeleteCollection(CollectionName);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _orchestrate = new Orchestrate(ApiKey);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // nothing to see here...
+        }
+
+        #region Ref Tests
+
+        [TestMethod]
+        public void GetByRef()
+        {
+            var list = _orchestrate.List(CollectionName, 10, null, null);
+            var match = list.Results.Single(x => x.Path.Key == "1");
+
+            var result = _orchestrate.Get(CollectionName, "1", match.Path.Ref);
+
+            Assert.IsTrue(result.Value != null);
+        }
+
+        [TestMethod]
+        public void GetByRefWithNoCollectionName()
+        {
+            try
+            {
+                _orchestrate.Get(string.Empty, "9999", "refernce#");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.IsTrue(ex.ParamName == "collectionName");
+                return;
+            }
+
+            Assert.Fail("No Exception Thrown");
+        }
+
+        [TestMethod]
+        public void GetByRefWithNoKey()
+        {
+            try
+            {
+                _orchestrate.Get(CollectionName, string.Empty, "refernce#");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.IsTrue(ex.ParamName == "key");
+                return;
+            }
+
+            Assert.Fail("No Exception Thrown");
+        }
+
+        [TestMethod]
+        public void GetByRefWithNoRef()
+        {
+            try
+            {
+                _orchestrate.Get(CollectionName, "9999", string.Empty);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.IsTrue(ex.ParamName == "reference");
+                return;
+            }
+
+            Assert.Fail("No Exception Thrown");
+        }
+
+        #endregion
+    }
+}
